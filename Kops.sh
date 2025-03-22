feb1cd0b52945ebff25b691129a4d8233f81a2af
YashPatil1678 <yashp1678@gmail.com>
2025-03-22 14:50:12 +0530

    Update Kops.sh

--- Kops.sh
+++ Kops.sh
@@ -11,8 +11,13 @@ chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
+# Create an S3 bucket & enable Versioning
 aws s3api create-bucket --bucket yashkops123.k8s.local --region ap-south-1 --create-bucket-configuration LocationConstraint=ap-south-1
 aws s3api put-bucket-versioning --bucket yashkops123.k8s.local --region ap-south-1 --versioning-configuration Status=Enabled
+
+# Set the KOPS_STATE_STORE environment variable to the S3 bucket where Kops will store the cluster state and configuration
 export KOPS_STATE_STORE=s3://yashkops123.k8s.local
-kops create cluster --name yashkops123.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+
+# Will create a cluster having 1 master node and 2 worker/slave nodes of t2.micro size
+kops create cluster --name yashkops123.k8s.local --zones ap-south-1a --master-count=1 --master-size t2.micro --node-count=2 --node-size t2.micro
 kops update cluster --name yashkops123.k8s.local --yes --admin
